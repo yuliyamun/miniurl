decfb56dbe25c96db55b8829321cb31be1f82f56
yuliyamun <ext.yuliya.mun@kesko.fi>
2023-05-10 13:10:23 +0300

    Implement api test for different responces

--- api/api.go
+++ api/api.go
@@ -30,18 +30,32 @@ type AddUrlResp struct {
 	Hash string `json:"hash"`
 }
 
+type ErrorResp struct {
+	Msg string `json:"msg"`
+}
+
 func (a *API) AddUrl(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	var v AddUrlReq
 	err := json.NewDecoder(r.Body).Decode(&v)
 	if err != nil {
-		//TODO:
+		resp := ErrorResp{Msg: "bad request"}
+		respondJSON(w, http.StatusBadRequest, resp)
 		return
 	}
 	hash, err := a.handler.AddUrl(v.Url)
 	if err != nil {
+		//TODO
 		return
 	}
 	err = json.NewEncoder(w).Encode(AddUrlResp{Url: v.Url, Hash: hash})
+	if err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func respondJSON(w http.ResponseWriter, code int, resp any) {
+	w.WriteHeader(code)
+	err := json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		slog.Error(err.Error())
 	}
--- api/api_test.go
+++ api/api_test.go
@@ -14,23 +14,45 @@ import (
 )
 
 func TestAPI_AddUrl(t *testing.T) {
-	const (
-		payload            = `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE"}`
-		expectedBody       = `{"url":"https://github.com/gourses/miniurl/blob/main/LICENSE", "hash":"testvalue"}`
-		expectedStatusCode = http.StatusOK
-	)
-	req := httptest.NewRequest(http.MethodPost, "/api/v1/url", strings.NewReader(payload))
-	rr := httptest.NewRecorder()
+	test := []struct {
+		name               string
+		handler            api.Handler
+		payload            string
+		expectedStatusCode int
+		expectedBody       string
+	}{
+		{
+			name:               "OK",
+			handler:            &strHandler{str: "testvalue"},
+			payload:            `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE"}`,
+			expectedBody:       `{"url":"https://github.com/gourses/miniurl/blob/main/LICENSE", "hash":"testvalue"}`,
+			expectedStatusCode: http.StatusOK,
+		},
+		{
+			name:               "Bad request",
+			handler:            nil,
+			payload:            `invalid json data`,
+			expectedBody:       `{"msg":"bad request"}`,
+			expectedStatusCode: http.StatusBadRequest,
+		},
+	}
+	for _, tc := range test {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, "/api/v1/url", strings.NewReader(tc.payload))
+			rr := httptest.NewRecorder()
 
-	r := httprouter.New()
-	h := &strHandler{str: "testvalue"}
-	api.Bind(r, h)
-	r.ServeHTTP(rr, req)
+			r := httprouter.New()
+			h := &strHandler{str: "testvalue"}
+			api.Bind(r, h)
+			r.ServeHTTP(rr, req)
 
-	assert.Equal(t, expectedStatusCode, rr.Result().StatusCode)
-	body, err := io.ReadAll(rr.Result().Body)
-	require.NoError(t, err)
-	assert.JSONEq(t, expectedBody, string(body))
+			assert.Equal(t, tc.expectedStatusCode, rr.Result().StatusCode)
+			body, err := io.ReadAll(rr.Result().Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, tc.expectedBody, string(body))
+		})
+
+	}
 }
 
 type strHandler struct {
